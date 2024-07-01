cab245e19ebf7db582573dbee1a9ec2fcfdee112
dalictn <dalitsocae@icloud.com>
2024-07-01 17:18:50 +0200

    add while loop in attempt to get continuous runtime

--- pyclip.py
+++ pyclip.py
@@ -8,74 +8,74 @@ from typing_extensions import Annotated
 
 
 def main(ip: Annotated[Optional[str], typer.Argument()] = None, port: Annotated[Optional[int], typer.Argument()] = None):
+
+    while True:
     
-    
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 
-    #create server function
-    def CreateServer():
+        #create server function
+        def CreateServer():
 
 
-        #default port
-        ip = ''
-        port = 11223
+            #default port
+            ip = ''
+            port = 12223
 
 
-
-
-
-        try:
-        # With the help of bind() function 
-        # binding ip and port
-            soc.bind((ip, port))
-        
-        except socket.error as message:
+            try:
+            # With the help of bind() function 
+            # binding ip and port
+                soc.bind((ip, port))
             
-            # if any error occurs then with the 
-            # help of sys.exit() exit from the program
-            print('[?] Bind failed. Error Code : '
-                + str(message[0]) + ' Message '
-                + message[1])
-            sys.exit()
+            except socket.error as message:
+                
+                # if any error occurs then with the 
+                # help of sys.exit() exit from the program
+                print('[?] Bind failed. Error Code : '
+                    + str(message[0]) + ' Message '
+                    + message[1])
+                sys.exit()
+                
+
+            print("[!] clipboard created. Now listening for connections on port: " + str(port))
+
+            soc.listen(9)
             
+            while True:
 
-        print("[!] clipboard created. Now listening for connections on port: " + str(port))
+                conn, address = soc.accept()            
+                
 
-        soc.listen(9)
+                print("[!] "+ address[0] + " connected")
+                conn.send('Clippy!'.encode()) 
+                
+                return True
+
+
+        def ConnectToServer(ip, port):
+            #host = (ip, port)
+            soc.connect((ip, port))
+
+            return True
+
+
+
+
+        def SendText(conn):
+
+            while True:
+                conn.send(pyperclip.waitForNewPaste())
+
+
         
+        if ip == None:
+            CreateServer()
+        else:
+            ConnectToServer(ip, port)
+
         
-        conn, address = soc.accept()
-        print("[!] "+ address[0] + " connected")
-
-
-                    
-        # send a thank you message to the client. encoding to send byte type. 
-        conn.send('Clippy!'.encode()) 
-        
-        return conn
-
-    def ConnectToServer(ip, port):
-        soc.connect(ip, port)
-
-
-
-
-
-    def SendText(conn):
-
-        while True:
-            conn.send(pyperclip.waitForNewPaste())
-
-
-    
-    if ip == None:
-        CreateServer()
-    else:
-        ConnectToServer(ip, port)
-
-    
 
 
 
